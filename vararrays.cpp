39df83c50405cd12fa77f21cf52e9c9d6894abd3
jlafleur93 <josephlafleur@hotmail.com>
2022-02-27 14:12:57 -0700

    switching to laptop to complete

---
+++ vararrays.cpp
@@ -0,0 +1,19 @@
+// a[0] = [1, 5, 4];
+// j = 1 therefore we print  a[1] which is 5'
+// find array located at index i = 1 which = a[1] = [1, 2, 8, 9, 3]
+//we must print index at j = 3 which is 9 in this case
+//basically we look for two values the first sets the variable sized arr while the second input is the 
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    vector <int> ret;
+      
+    return 0;
+}
