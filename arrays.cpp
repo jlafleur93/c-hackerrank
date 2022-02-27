e3296a4a374947da583c780ece3ddafbaf1b425f
joelwowz <josephlafleur@hotmail.com>
2022-02-26 20:05:08 -0700

    switching to windows as cin doesnt work on macos

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int a;
+  cout << "enter numb";
+  cin >> a;
+  return 0;
+}
