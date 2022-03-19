3fcbe576cfe0da4e05b72fcce426d0a67107dc40
jlafleur93 <josephlafleur@hotmail.com>
2022-03-19 01:30:12 -0600

    attempting class challenge

---
+++ class.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+/*
+Enter code for class Student here.
+Read statement for specification.
+*/
+class Student {
+    private:
+    int age, standard;
+    string first_name, last_name;
+    public:
+    void set_age(int a){
+        age = a;
+    }
+    int get_age(){
+        return age;
+    }
+    void set_standard(int b){
+        standard = b;
+    }
+    int get_standard(){
+        return standard;
+    }
+    string get_last_name(){
+        return last_name;
+    }
+    void set_last_name(string n){
+        last_name  = n;
+    }
+    void set_first_name(string name){
+        first_name = name;
+    }
+    string get_first_name(){
+        return first_name;
+    }
+    string to_string(){
+         std::stringstream ss;
+       
+    }
+};
+int main() {
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
+    return 0;
+}
